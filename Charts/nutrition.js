d69bc95bc3dd206193660b59fd5c4fee7c420a75
len-nguyen <70462461+len-nguyen@users.noreply.github.com>
2021-11-09 15:40:47 -1000

    Layout of bar graph is done, just need to fill out values

--- Charts/nutrition.js
+++ Charts/nutrition.js
@@ -34,6 +34,8 @@ function processData(allRows){
 
   }
   console.log("TEST", Name);
+  console.log("Sodium", SodiumContent);
+
 
 }
 
@@ -58,7 +60,7 @@ let nutrition = {
       borderColor: 'none',
       size: '10px'
     },
-    maxItems: 6,
+    maxItems: 3,
     overflow: 'page',
     pageOff: {
       alpha: 0.65,
@@ -90,7 +92,11 @@ let nutrition = {
   scrollX: {
     bar: {
       backgroundColor: '#eee',
-      height: '10px'
+      height: '10px',
+      alpha: 0.5
+    },
+    handle: {
+      backgroundColor: "#7e7e7e",
     }
   },
   scaleX: {
@@ -98,15 +104,16 @@ let nutrition = {
       visible: false
     },
     item: {
-      fontColor: '#7e7e7e'
+      fontColor: '#7e7e7e',
+      fontAngle: 15,
     },
-    labels: ['Q3-20', 'Q4-20', 'Q1-21', 'Q2-21'],
+    labels: ["Quick Pasta Supper", 'Q4-20', 'Q1-21', 'Q2-21'],
     lineColor: '#7E7E7E',
     zooming: true,
-    zoomSnap: true,
+    zoomTo: [0, 10],
   },
   scaleY: {
-    values: '0:60:10',
+    values: '0:200:10',
     guide: {
       visible: true
     },
@@ -114,63 +121,84 @@ let nutrition = {
       fontColor: '#7e7e7e'
     },
     label: {
-      text: '$ Billions',
+      text: 'Nutrition (g)',
       bold: true,
       fontAngle: 0,
       fontColor: '#7E7E7E',
       fontFamily: 'arial',
-      fontSize: '14px',
+      fontSize: '12px',
       offsetX: '20px',
       offsetY: '-190px'
     },
     lineColor: '#7E7E7E'
   },
   tooltip: {
-    text: '$%v Billion'
+    text: '%kl <br> %v (g) %t'
   },
 
   series: [
     {
-      text: 'Apple',
-      values: [37.47, 57.59, 45.65, 37.43, 37.47, 57.59, 45.65, 37.43, 37.47, 57.59, 45.65, 37.43, 37.47, 57.59, 45.65, 37.43],
-      alpha: 0.95,
-      borderRadiusTopLeft: '7px',
-      backgroundColor: '#8993c7'
-    },
-    {
-      text: 'Facebook',
+      text: 'Calories',
       values: [2.02, 2.59, 2.5, 2.91, 37.47, 57.59, 45.65, 37.43, 37.47, 57.59, 45.65, 37.43, 37.47, 57.59, 45.65, 37.43],
       borderRadiusTopLeft: '7px',
       alpha: 0.95,
       backgroundColor: '#fdb462'
     },
     {
-      text: 'Google',
-      values: [13.4, 14.11, 14.89, 16.86, 13.4, 14.11, 14.89, 16.86, 13.4, 14.11, 14.89, 16.86, 13.4, 14.11, 14.89, 16.86],
+      text: 'Fat Content',
+      values: [13.4, 14.11, 14.89, 16.86, 13.4, 14.11, 14.89, 16.86, 13.4, 14.11, 14.89, 16.86, 13.4, 14.11, 14.89, 100],
       alpha: 0.95,
       borderRadiusTopLeft: '7px',
       backgroundColor: '#8dd3c7'
     },
     {
-      text: 'Microsoft',
+      text: 'Saturated Fat Content',
       values: [13.4, 14.11, 14.89, 16.86, 13.4, 14.11, 14.89, 16.86, 13.4, 14.11, 14.89, 16.86, 13.4, 14.11, 14.89, 16.86],
       borderRadiusTopLeft: '7px',
       alpha: 0.95,
       backgroundColor: '#fb8072'
     },
     {
-      text: 'Amazon',
+      text: 'Cholesterol Content',
       values: [13.4, 14.11, 14.89, 16.86, 13.4, 14.11, 14.89, 16.86, 13.4, 14.11, 14.89, 16.86, 13.4, 14.11, 14.89, 16.86],
       borderRadiusTopLeft: '7px',
       alpha: 0.95,
       backgroundColor: '#80b1d3'
     },
     {
-      text: 'Cognizant',
+      text: 'Sodium Content',
       values: [13.4, 14.11, 14.89, 16.86, 13.4, 14.11, 14.89, 16.86, 13.4, 14.11, 14.89, 16.86, 13.4, 14.11, 14.89, 16.86],
       borderRadiusTopLeft: '7px',
       alpha: 0.95,
       backgroundColor: '#b3de69'
+    },
+    {
+      text: 'Carbohydrate Content',
+      values: [13.4, 14.11, 14.89, 16.86, 13.4, 14.11, 14.89, 16.86, 13.4, 14.11, 14.89, 16.86, 13.4, 14.11, 14.89, 16.86],
+      borderRadiusTopLeft: '7px',
+      alpha: 0.95,
+      backgroundColor: '#8993c7'
+    },
+    {
+      text: 'Fiber Content',
+      values: [13.4, 14.11, 14.89, 16.86, 13.4, 14.11, 14.89, 16.86, 13.4, 14.11, 14.89, 16.86, 13.4, 14.11, 14.89, 16.86],
+      borderRadiusTopLeft: '7px',
+      alpha: 0.95,
+      backgroundColor: '#de69c7'
+    },
+    {
+      text: 'Sugar Content',
+      values: [13.4, 14.11, 14.89, 16.86, 13.4, 14.11, 14.89, 16.86, 13.4, 14.11, 14.89, 16.86, 13.4, 14.11, 14.89, 16.86],
+      borderRadiusTopLeft: '7px',
+      alpha: 0.95,
+      backgroundColor: '#b369de'
+    },
+    {
+      text: 'Protein Content',
+      values: [13.4, 14.11, 14.89, 16.86, 13.4, 14.11, 14.89, 16.86, 13.4, 14.11, 14.89, 16.86, 13.4, 14.11, 14.89, 16.86],
+      borderRadiusTopLeft: '7px',
+      alpha: 0.95,
+      backgroundColor: '#dade69'
     }
 
 
