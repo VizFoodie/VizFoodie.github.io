9fc0564f09b591120f889d9130e4bc036af981b6
Jonathan-Ma <jma66@hawaii.edu>
2021-11-23 18:07:58 -1000

    menu for index page

---
+++ Charts/menu.js
@@ -0,0 +1,27 @@
+Plotly.d3.csv("CSV/recipesNutrition.csv", function (err, rows) {
+  function unpack(rows, key) {
+    return rows.map(function (row) { return row[key]; });
+  }
+
+  var values = []
+  values.push(unpack(rows, 'Name'))
+
+  var data = [{
+    type: 'table',
+    header: {
+      values: [["<b>Name</b>"]],
+      align: "center",
+      line: {width: 1, color: 'black'},
+      fill: {color: "grey"},
+      font: {family: "Arial", size: 12, color: "white"}
+    },
+    cells: {
+      values: values,
+      align: "center",
+      line: {color: "black", width: 1},
+      font: {family: "Arial", size: 11, color: ["black"]}
+    }
+  }]
+
+  Plotly.newPlot('menu', data);
+});
